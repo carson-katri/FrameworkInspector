ddd4bcc437733b0a31427305ff0d9ffdc2ff5805
Carson Katri <carson.katri@gmail.com>
2020-12-18 21:59:50 -0500

    Initial Commit

---
+++ FrameworkInspector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FrameworkInspector
+//
+//  Created by Carson Katri on 12/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FrameworkInspector/FrameworkInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkInspectorApp.swift
+//  FrameworkInspector
+//
+//  Created by Carson Katri on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct FrameworkInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
